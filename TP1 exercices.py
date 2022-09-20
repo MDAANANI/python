3b9dde6f411991b35aeda6be1eda8724c6a0cfcb
MDAANANI <113105580+MDAANANI@users.noreply.github.com>
2022-09-20 08:54:48 +0200

    Add files via upload

---
+++ TP1 exercices.py
@@ -0,0 +1,46 @@
+'''
+a=4
+carre= a**2
+print("La valeur de a=",a,"au carre est de",carre)
+'''
+'''
+kilometre=100
+miles=kilometre/1609
+print("la distance de ",kilometre,"kilometre est de ",miles,"miles")
+'''
+'''
+a=2
+b=5
+
+print("Avant échange a=",a," et b=",b)
+temp=a
+a=5
+b=2
+
+print("Apres échange a=",a,"et b=",b)
+'''
+'''
+valeur_tva=20
+prix_ht=40
+tva=prix_ht*valeur_tva/100;
+prix_ttc=prix_ht+tva;
+
+print("Prix HT du produit:",prix_ht,"euros")
+print("Indice de la TVA en 2015:",valeur_tva,"%")
+print("Valeur de la TVA:",tva,"euros")
+print("Prix TTC:",prix_ttc,"euros")
+'''
+
+a=3
+b=15
+r=a*b
+if r>0:
+    print("le produit est positif")
+elif r<0:
+    print("le produit est negatif")
+else:
+    print("ton produit est egal a 0")
+
+
+
+
