3412f3e4f35ec11b88f3e21ec8b6b2968085189f
MDAANANI <113105580+MDAANANI@users.noreply.github.com>
2022-09-20 09:31:35 +0200

    Add files via upload

---
+++ algo/tp2/TP2 exercices.py
@@ -0,0 +1,14 @@
+# Créé par mohamed.daanani, le 20/09/2022 en Python 3.7
+'''
+#1
+for n in range(16):
+    print(n)
+'''
+'''
+#2
+for n in range(15,-1,-1):
+    print(n)
+'''
+
+#3
+d=int(input("valeur de debut:"))
