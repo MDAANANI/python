3d1bd637aac8ccdf58f1a92fbf08e08544c5acbd
MDAANANI <113105580+MDAANANI@users.noreply.github.com>
2023-05-30 08:43:10 +0200

    Add files via upload

---
+++ algo/Partie 1/TP1 exercices.py
@@ -0,0 +1,70 @@
+'''
+a=4
+carre= a**2
+print("La valeur de a=",a,"au carre est de",carre)
+'''
+'''
+kilometre=100
+miles=kilometre/1609
+print("la distance de ",kilometre,"kilometre est de ",miles,"miles")
+'''
+'''
+a=2
+b=5
+
+print("Avant échange a=",a," et b=",b)
+temp=a
+a=5
+b=2
+
+print("Apres échange a=",a,"et b=",b)
+'''
+'''
+valeur_tva=20
+prix_ht=40
+tva=prix_ht*valeur_tva/100;
+prix_ttc=prix_ht+tva;
+
+print("Prix HT du produit:",prix_ht,"euros")
+print("Indice de la TVA en 2015:",valeur_tva,"%")
+print("Valeur de la TVA:",tva,"euros")
+print("Prix TTC:",prix_ttc,"euros")
+'''
+'''
+#5
+a=3
+b=15
+r=a*b
+if r>0:
+    print("le produit est positif")
+elif r<0:
+    print("le produit est negatif")
+else:
+    print("ton produit est egal a 0")
+'''
+
+'''
+#6
+age=14
+if age>=6 & age<=7:
+    print("poussin")
+elif age>=8 & age<=9:
+    print("pupille")
+elif age>=10 & age<=11:
+    print("minime")
+elif age>=12 & age<=15:
+    print("cadet")
+else print("hors categorie")
+'''
+'''
+#7
+nb_valeur=10
+result=0
+temp=0
+for i in range(0,nb_valeur)
+    temp=temp+1
+    result= temp + result
+    print("somme des",nb_valeur,"premiers nombre entier est:", result)
+'''
+
+
